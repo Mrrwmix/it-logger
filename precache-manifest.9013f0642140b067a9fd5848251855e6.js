881e47d7fa4b34c5e689396f7af46e09842d7f30
Matthew Winemiller <seidoc@gmail.com>
2019-09-01 23:35:43 -0700

    Updates

---
+++ precache-manifest.9013f0642140b067a9fd5848251855e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bf0a80a1c19cbb04ba977fd612eca7a",
+    "url": "/it-logger/index.html"
+  },
+  {
+    "revision": "59b8ed3f57a285f8142f",
+    "url": "/it-logger/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "f86ba0418e3df87172af",
+    "url": "/it-logger/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "59b8ed3f57a285f8142f",
+    "url": "/it-logger/static/js/2.f7cbf278.chunk.js"
+  },
+  {
+    "revision": "f86ba0418e3df87172af",
+    "url": "/it-logger/static/js/main.f9d07e0d.chunk.js"
+  },
+  {
+    "revision": "377f36e1422ce0b620db",
+    "url": "/it-logger/static/js/runtime~main.7113cb1b.js"
+  }
+]);
